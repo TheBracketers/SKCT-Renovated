f46db8ac19074e8eb4d8d15d086543be14a7086b
aslamcodes <mohamedaslammn.2003@gmail.com>
2021-08-13 07:16:57 +0530

    Template for Hovers

--- common/UI/navbar-hover.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-function NavbarHoverer() {
-  return <div className="w-full h-20">Hello</div>;
-}
-
-export default NavbarHoverer;
--- common/UI/navbar.tsx
+++ common/UI/navbar.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 
 import Link from 'next/link';
 import { useState } from 'react';
-import NavbarHoverer from './navbar-hover';
 
 function Navbar() {
   const [showLinkLists, setShowLinkLists] = useState(false);
@@ -20,13 +19,13 @@ function Navbar() {
 
   return (
     <div className="relative">
-      <div className="shadow-lg p-0 m-0">
+      <div className="p-0 m-0">
         <div className="flex justify-between items-center px-10 py-2 md:py-4 lg:px-16 lg:py-4">
           <div className="font-semibold text-3xl cursor-pointer">
             <Link href="/">SKCT</Link>
           </div>
 
-          <div className="hidden lg:flex flex-col gap-8">
+          <div className="hidden lg:flex flex-col ">
             <nav className="hidden md:flex gap-5 justify-end text-gray-500 font-bold text-xs uppercase ">
               <a className="hover:underline  transition duration-300 cursor-pointer">
                 Students
@@ -60,59 +59,76 @@ function Navbar() {
               </a>
             </nav>
             <nav className="hidden md:flex gap-8 justify-center items-center font-bold text-lg">
-              <Link href="/about">
-                <a
-                  className="hover:text-blue-500 transition duration-300"
-                  onMouseOver={onMouseEntered}>
-                  About
-                </a>
-              </Link>
+              <div className="group py-7 ">
+                <Link href="/about">
+                  <a
+                    className="group-hover:text-blue-500 transition duration-300"
+                    onMouseOver={onMouseEntered}>
+                    About
+                  </a>
+                </Link>
+                <div className="group-hover:flex hidden absolute  left-0  w-full ">
+                  <div className=" text-white block w-full bg-black mt-9 px-16 py-4">
+                    <a className="hover:text-blue-500 transition duration-300">
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                      Veritatis odio similique aliquid? Ad illo dolorem
+                      voluptatem unde beatae nobis at similique quae
+                      perspiciatis est? Rem at eos saepe veniam explicabo?
+                    </a>
+                  </div>
+                </div>
+              </div>
 
               <Link href="/academics">
-                <a className="hover:text-blue-500 transition duration-300">
+                <a className="hover:text-blue-500 transition duration-300 py-7  ">
                   Academics
                 </a>
               </Link>
               <Link href="/R&D">
-                <a className="hover:text-blue-500 transition duration-300">
+                <a className="hover:text-blue-500 transition duration-300 py-7  ">
                   Research & Work
                 </a>
               </Link>
               <Link href="/campus-life">
-                <a className="hover:text-blue-500 transition duration-300">
+                <a className="hover:text-blue-500 transition duration-300 py-7  ">
                   Campus Life
                 </a>
               </Link>
               <Link href="/outcomes">
-                <a className="hover:text-blue-500 transition duration-300">
+                <a className="hover:text-blue-500 transition duration-300 py-7  ">
                   Outcomes
                 </a>
               </Link>
               <Link href="/admissions">
-                <a className="hover:text-blue-500 transition duration-300">
+                <a className="hover:text-blue-500 transition duration-300 py-7  ">
                   Admission
                 </a>
               </Link>
-              <button onClick={onSearch}>ICON </button>
+              <button onClick={onSearch} className="py-7">
+                ICON
+              </button>
             </nav>
           </div>
-          <div className="lg:hidden flex justify-between items-center gap-6">
+          <div className="lg:hidden flex justify-between items-center gap-6 ">
             <button onClick={onSearch}>ICON </button>
             <button onClick={onExtend}>ICON </button>
           </div>
         </div>
       </div>
       {showLinkLists && (
-        <div className="absolute z-10 lg:hidden">
-          Toggled
+        <div className="absolute z-10 lg:hidden bg-black text-white ">
           {/* Todo:  Jayvan - Create a component that lists all the links*/}
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Error, beatae
+          sapiente tempore voluptates voluptatem necessitatibus voluptate,
+          dolore, quas est voluptatibus quisquam aliquam harum? Saepe est
+          ducimus enim voluptatum vero animi.
         </div>
       )}
-      {isHovered && (
+      {/* {isHovered && (
         <div
           className="hidden lg:block absolute z-10 bg-black text-blue-500 w-full"
           id="HoverArea"></div>
-      )}
+      )} */}
     </div>
   );
 }
