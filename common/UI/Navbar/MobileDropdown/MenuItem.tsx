0e74235b22d55d40f4172801e92fca57b553390f
aslamcodes <mohamedaslammn.2003@gmail.com>
2021-08-15 09:27:03 +0530

    Add sub-dropdown for mobile

--- common/UI/Navbar/MobileDropdown/MenuItem.tsx
+++ common/UI/Navbar/MobileDropdown/MenuItem.tsx
@@ -1,19 +1,48 @@
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
 import Link from 'next/link';
-import { BsChevronDown } from 'react-icons/bs';
+import { BsChevronDown, BsChevronUp } from 'react-icons/bs';
 const MenuItem: FC<{ href: String; name: String }> = ({ href, name }) => {
-  return (
-    <div className="group   pt-6 px-3 flex justify-between">
-      <Link href={'/' + href}>
-        <a className=" align-middle group-hover:text-blue-500 transition duration-300 ">
-          {name}
-        </a>
-      </Link>
+  const [isTouched, setIsTouched] = useState(false);
 
-      <div className="transform rotate-0 transition duration-300 group-hover:rotate-180">
-        <BsChevronDown />
+  return (
+    <>
+      <div
+        className="group pt-6 px-3 flex justify-between"
+        onClick={() => {
+          console.log('Hello');
+          setIsTouched((prev) => !prev);
+        }}>
+        <Link href={'/' + href}>
+          <a className="align-middle group-hover:text-blue-500 transition duration-300">
+            {name}
+          </a>
+        </Link>
+        <div
+          className={
+            isTouched
+              ? 'transform transition duration-300 -rotate-0'
+              : 'transform transition duration-300 rotate-180'
+          }>
+          <BsChevronDown />
+        </div>
       </div>
-    </div>
+      {isTouched && (
+        <div className="px-3">
+          <p>
+            I Think of we need to pass a component that contains all sublinks,
+            that'll be rendendered conditionally
+          </p>
+          <p>
+            I Think of we need to pass a component that contains all sublinks,
+            that'll be rendendered conditionally
+          </p>
+          <p>
+            I Think of we need to pass a component that contains all sublinks,
+            that'll be rendendered conditionally
+          </p>
+        </div>
+      )}
+    </>
   );
 };
 
