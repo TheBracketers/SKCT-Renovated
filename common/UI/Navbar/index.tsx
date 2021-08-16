9255be6cff462fba36938a5d3d389bf52bb8486c
aslamcodes <mohamedaslammn.2003@gmail.com>
2021-08-16 08:10:09 +0530

    Remove Relative positioning

--- common/UI/Navbar/index.tsx
+++ common/UI/Navbar/index.tsx
@@ -27,8 +27,8 @@ function Navbar() {
   };
 
   return (
-    <div className="relative mb-2 overflow-auto ipad-pro:overflow-visible">
-      <div className="  w-full bg-white ">
+    <div className="mb-2 overflow-auto ipad-pro:overflow-visible">
+      <div className="w-full bg-white ">
         <AnimateHeight height={showSearch ? 'auto' : 0}>
           <SearchBar
             onClick={() => {
