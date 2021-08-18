538b4943d9e6648f5945dd13d3acaee47dd084d2
aslamcodes <mohamedaslammn.2003@gmail.com>
2021-08-18 17:19:12 +0530

    Scroll To top button

--- common/components/Layout.tsx
+++ common/components/Layout.tsx
@@ -1,12 +1,14 @@
 import React, { Fragment } from 'react';
 import Footer from '../UI/footer';
 import Navbar from '../UI/Navbar';
+import ScrollToTopButton from './ScrollToTopButton';
 
 function Layout({ children }) {
   return (
     <div className='absolute top-0 z-50'>
       <Navbar />
       <main>{children}</main>
+      <ScrollToTopButton />
       <Footer />
     </div>
   );
