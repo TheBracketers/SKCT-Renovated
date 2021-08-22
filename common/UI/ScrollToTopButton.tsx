fdf88f0a6bad931ebc1f7bb51f8d3875e992f741
aslamcodes <mohamedaslammn.2003@gmail.com>
2021-08-22 18:49:53 +0530

    Fix ScrollToTop button

--- common/UI/ScrollToTopButton.tsx
+++ common/UI/ScrollToTopButton.tsx
@@ -1,8 +1,21 @@
 import React, { useEffect, useState } from 'react';
 import { FaArrowUp } from 'react-icons/fa';
+import CSSTransition from 'react-transition-group/CSSTransition';
 
 function ScrollToTopButton() {
   const [showButton, setShowButton] = useState(false);
+
+  useEffect(() => {
+    window.onscroll = () => {
+      console.log(window.pageYOffset);
+
+      if (window.pageYOffset > 150) {
+        setShowButton(true);
+      } else {
+        setShowButton(false);
+      }
+    };
+  });
 
   const scrollToTop = () => {
     window.scrollTo({
@@ -13,11 +26,18 @@ function ScrollToTopButton() {
 
   return (
     <div>
-      <button
-        onClick={scrollToTop}
-        className='fixed z-50 text-white text-2xl rounded-full p-3 opacity-10 hover:opacity-100 transition duration-300  bg-black  bottom-5 right-5'>
-        <FaArrowUp />
-      </button>
+      <CSSTransition
+        in={showButton}
+        mountOnEnter
+        unmountOnExit
+        timeout={600}
+        classNames='fade-in-out'>
+        <button
+          onClick={scrollToTop}
+          className='fixed z-50 text-white text-2xl rounded-full p-3 opacity-10 hover:opacity-100 transition duration-300  bg-black  bottom-5 right-5'>
+          <FaArrowUp />
+        </button>
+      </CSSTransition>
     </div>
   );
 }
